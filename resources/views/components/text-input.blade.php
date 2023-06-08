c6086bb8f322cfc1f853a89f65fd4f1403f2b8b6
kartikStegpearl <133215576+kartikStegpearl@users.noreply.github.com>
2023-06-08 13:01:38 +0530

    dark mode removed

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,3 +1,3 @@
 @props(['disabled' => false])
 
-<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-gray-200 dark:border-gray-300 dark:bg-gray-100 dark:text-gray-700 focus:border-indigo-200 dark:focus:border-indigo-200 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm']) !!}>
+<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-gray-200focus:border-indigo-200  focus:ring-indigo-500 rounded-md shadow-sm']) !!}>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-800 leading-tight">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('YooMail') }}
         </h2>
     </x-slot>
@@ -22,14 +22,14 @@
           </div>
             <table class="table-auto w-full border-collapse border">
                 <tbody>
-                    <tr class="border hover:bg-gray-100 dark:hover:bg-gray-100">
+                    <tr class="border hover:bg-gray-100 ">
                         <td class="p-2"><Input type="checkbox"></td>
                         <td class="p-2">Account Confirmation State Bank...</td>
                         <td class="p-2">Re: Es gibt neues zu Ihre Anfrage [#12556] bei dem GLS Support - Anfrage
                             Kontaktformular...</td>
                         <td class="p-2">11:01 am</td>
                     </tr>
-                    <tr class=" border hover:bg-gray-100 dark:hover:bg-gray-100">
+                    <tr class=" border hover:bg-gray-100 ">
                         <td class="p-2"><Input type="checkbox"></td>
                         <td class="p-2">Bank Statment</td>
                         <td class="p-2">vielen Dank, dass Sie sich als Händler bei Solar Hook</td>
@@ -46,24 +46,22 @@
 //Composer
 <div id="composeModal" class="fixed inset-0 flex items-center justify-center z-10 hidden">
 
-        <div class="bg-white dark:bg-gray-800 p-6 rounded-lg shadow-lg">
+        <div class="bg-white p-6 rounded-lg shadow-lg w-2/4">
             <div class="flex items-center">
-                <div class="flex-shrink-0">
-                    <img class="h-10 w-10 rounded-full" src="/path/to/profile-picture.jpg" alt="Profile Picture">
-                </div>
+
                 <div class="ml-4">
-                    <h2 class="text-lg font-semibold text-gray-900 dark:text-gray-100">Compose Email</h2>
-                    <p class="text-sm text-gray-500 dark:text-gray-400">From: <span class="font-medium">Your Name</span></p>
+                    <h2 class="text-lg font-semibold text-gray-900">Compose Email</h2>
+                    <p class="text-sm text-gray-500">From: <span class="font-medium">{{auth()->user()->new_email}}</span></p>
                 </div>
             </div>
             <div class="mt-6">
-                <input type="text" class="w-full bg-gray-100 dark:bg-gray-700 text-gray-900 dark:text-gray-100 rounded-lg px-4 py-3 focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="To">
+                <input type="text" class="w-full bg-gray-100  text-gray-900 rounded-lg px-4 py-3 focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="To">
             </div>
             <div class="mt-4">
-                <input type="text" class="w-full bg-gray-100 dark:bg-gray-700 text-gray-900 dark:text-gray-100 rounded-lg px-4 py-3 focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="Subject">
+                <input type="text" class="w-full bg-gray-100  text-gray-900  rounded-lg px-4 py-3 focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="Subject">
             </div>
             <div class="mt-4">
-                <textarea class="w-full bg-gray-100 dark:bg-gray-700 text-gray-900 dark:text-gray-100 rounded-lg px-4 py-3 focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="Message" rows="6"></textarea>
+                <textarea class="w-full bg-gray-100  text-gray-900  rounded-lg px-4 py-3 focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="Message" rows="6"></textarea>
             </div>
             <div class="mt-6 flex justify-end">
                 <button class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">Send</button>
